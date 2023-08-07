d89e010adc8f9d9b87ce0fc7afe57b0ec815e321
George Aggelou <g.aggelou@netstudio.gr>
2023-08-07 17:51:43 +0300

    Adds minor functionality and resolves box shadow issue with search input.

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -1,13 +1,23 @@
 <script setup>
   import { searchPokemon } from '../utils/pokemonUtils.js';
   import { usePokemonStore } from '../stores/pokemon.js';
-  import { computed } from 'vue';
+  import { computed, watch, ref } from 'vue';
+  import { useMagicKeys } from '@vueuse/core';
 
   const pokemonStore = usePokemonStore();
   const placeholderMsg = computed(() => pokemonStore.nameSearch
     ? 'Enter Pokemon Name to search by name'
     : 'Enter Pokemon Type to search by type'
   );
+
+  const searchInput = ref(null);
+
+  const keys = useMagicKeys()
+  const space = keys['Cmd+K']
+
+  watch(space, (v) => {
+    if (v) searchInput.value.focus()
+  })
 
   function setNameSearch() {
     pokemonStore.nameSearch = true;
@@ -22,6 +32,7 @@
 <template>
   <section aria-label="pokemon search">
     <input
+      ref="searchInput"
       type="text"
       :placeholder="placeholderMsg"
       id="pokemonSearch"
@@ -46,19 +57,20 @@
     display: grid;
     grid-template-columns: 1fr max-content max-content;
     padding-top: 1.5rem;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .13), 0 1px 1px 0 rgba(0, 0, 0, .11);
   }
 
   input {
     border: none;
     padding: .8rem 1.5rem;
     border-radius: .3rem 0 0 .3rem;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .13), 0 1px 1px 0 rgba(0, 0, 0, .11);
   }
 
   section button {
     border: none;
     cursor: pointer;
     padding: 0 .9rem;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .13), 0 1px 1px 0 rgba(0, 0, 0, .11);
   }
 
   section button:nth-child(3){
