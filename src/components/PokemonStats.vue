2e4aec001ed9085e6d0ffcb300c2db18e3192a3f
George Aggelou <g.aggelou@netstudio.gr>
2023-08-20 21:01:01 +0300

    Modularize stat display component.

---
+++ src/components/PokemonStats.vue
@@ -0,0 +1,101 @@
+<template>
+    <p>
+        <span>HP</span>
+        <span><span class="stat hp"></span></span>
+        <span>{{ stats.hp }}</span>
+    </p>
+    <p>
+        <span>ATK</span>
+        <span><span class="stat attack"></span></span>
+        <span>{{ stats.attack }}</span>
+    </p>
+    <p>
+        <span>SP ATK</span>
+        <span><span class="stat special_attack"></span></span>
+        <span>{{ stats.special_attack }}</span>
+    </p>
+    <p>
+        <span>DEF</span>
+        <span><span class="stat defense"></span></span>
+        <span>{{ stats.defense }}</span>
+    </p>
+    <p>
+        <span>SP DEF</span>
+        <span><span class="stat special_defense"></span></span>
+        <span>{{ stats.special_defense }}</span>
+    </p>
+    <p>
+        <span>SPD</span>
+        <span><span class="stat speed"></span></span>
+        <span>{{ stats.speed }}</span>
+    </p>
+</template>
+
+<style scoped>
+
+    p {
+        font-size: .8rem;
+        display: grid;
+        gap: .5rem;
+        grid-template-columns: max-content 1fr max-content;
+        align-items: center;
+    }
+
+    p :nth-child(1) {
+        font-weight: bold;
+        font-size: 1rem;
+    }
+
+    p :nth-child(2) span {
+        display: block;
+        height: .5rem;
+        border-radius: 2rem;
+    }
+
+    p:nth-child(2) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.hp), 100%);
+    }
+
+    p:nth-child(3) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.attack), 100%);
+    }
+
+    p:nth-child(4) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.special_attack), 100%);
+    }
+
+    p:nth-child(5) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.defense), 100%);
+    }
+
+    p:nth-child(6) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.special_defense), 100%);
+    }
+
+    p:nth-child(7) span:nth-child(2) span {
+        width: clamp(0%, v-bind(statStyles.speed), 100%);
+    }
+
+</style>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    stats: {
+        type: Object,
+        required: true
+    }
+})
+
+const statStyles = computed(() => {
+    return {
+        hp: `${props.stats.hp}px`,
+        attack: `${props.stats.attack}px`,
+        special_attack: `${props.stats.special_attack}px`,
+        defense: `${props.stats.defense}px`,
+        special_defense: `${props.stats.special_defense}px`,
+        speed: `${props.stats.speed}px`,
+    }
+})
+</script>
--- src/components/PokemonTeaser.vue
+++ src/components/PokemonTeaser.vue
@@ -15,41 +15,16 @@
                 {{ pokemon.name }}
                 <span>#{{ pokemon.id }}</span>
             </h3>
-            <p>
-                <span>HP</span>
-                <span><span class="stat hp"></span></span>
-                <span>{{ pokemon.hp }}</span>
-            </p>
-            <p>
-                <span>ATK</span>
-                <span><span class="stat attack"></span></span>
-                <span>{{ pokemon.attack }}</span>
-            </p>
-            <p>
-                <span>SP ATK</span>
-                <span><span class="stat special_attack"></span></span>
-                <span>{{ pokemon.special_attack }}</span>
-            </p>
-            <p>
-                <span>DEF</span>
-                <span><span class="stat defense"></span></span>
-                <span>{{ pokemon.defense }}</span>
-            </p>
-            <p>
-                <span>SP DEF</span>
-                <span><span class="stat special_defense"></span></span>
-                <span>{{ pokemon.special_defense }}</span>
-            </p>
-            <p>
-                <span>SPD</span>
-                <span><span class="stat speed"></span></span>
-                <span>{{ pokemon.speed }}</span>
-            </p>
+
+            <PokemonStats :stats="pokemonStats" />
         </div>
     </article>
 </template>
 
 <script setup>
+    import { computed } from 'vue'
+    import PokemonStats from './PokemonStats.vue';
+
     const props = defineProps({
         pokemon: {
             type: Object,
@@ -57,14 +32,16 @@
         }
     })
 
-    const stats = {
-        hp: `${props.pokemon.hp}px`,
-        attack: `${props.pokemon.attack}px`,
-        special_attack: `${props.pokemon.special_attack}px`,
-        defense: `${props.pokemon.defense}px`,
-        special_defense: `${props.pokemon.special_defense}px`,
-        speed: `${props.pokemon.speed}px`,
-    }
+    const pokemonStats = computed(() => {
+        return {
+            hp: props.pokemon.hp,
+            attack: props.pokemon.attack,
+            special_attack: props.pokemon.special_attack,
+            defense: props.pokemon.defense,
+            special_defense: props.pokemon.special_defense,
+            speed: props.pokemon.speed,
+        }
+    })
 
 </script>
 
@@ -111,49 +88,6 @@ div h3 span {
     color: #666;
     margin-left: 1rem;
     font-weight: normal;
-}
-
-div p {
-    font-size: .8rem;
-    display: grid;
-    gap: .5rem;
-    grid-template-columns: max-content 1fr max-content;
-    align-items: center;
-}
-
-div p :nth-child(1) {
-    font-weight: bold;
-    font-size: 1rem;
-}
-
-div p :nth-child(2) span {
-    display: block;
-    height: .5rem;
-    border-radius: 2rem;
-}
-
-div p:nth-child(2) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.hp), 100%);
-}
-
-div p:nth-child(3) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.attack), 100%);
-}
-
-div p:nth-child(4) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.special_attack), 100%);
-}
-
-div p:nth-child(5) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.defense), 100%);
-}
-
-div p:nth-child(6) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.special_defense), 100%);
-}
-
-div p:nth-child(7) span:nth-child(2) span {
-    width: clamp(0%, v-bind(stats.speed), 100%);
 }
 
 </style>
